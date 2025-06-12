16a14ec3a51459f66d33fbc05d43dea1b2fb526c
Prabash Lakshitha <prabashlakshitha35@gmail.com>
2025-06-12 15:24:16 +0530

    update

--- screen.py
+++ screen.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from pygame.locals import QUIT
 import random
-WIDTH = 640
+
 
 pygame.init()
 
@@ -16,9 +16,9 @@ red=(255,0,2)
 rabit_img=pygame.image.load("rabbit.png")
 rabit_img=pygame.transform.scale(rabit_img,(50,50))
 
-rabiit=pygame.Rect(200, 600, 50, 50)
-rabiit_speed_x=0
-rabiit_speed_y=1
+rabbit=pygame.Rect(100, 500, 50, 50)
+rabbit_speed_x=0
+rabbit_speed_y=1
 
 food = pygame.Rect(random.randint(0, screen.get_width()-30), -30, 30, 30)
 food_speed = 2
@@ -32,33 +32,33 @@ while True:
             
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                rabiit_speed_x = -5
+                rabbit_speed_x = -5
             elif event.key == pygame.K_RIGHT:
-                rabiit_speed_x = 5
+                rabbit_speed_x = 5
 
         if event.type == pygame.KEYUP:
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
-                rabiit_speed_x = 0
+                rabbit_speed_x = 0
 
-    rabiit.x += rabiit_speed_x
-    rabiit.y += rabiit_speed_y
+    rabbit.x += rabbit_speed_x
+    rabbit.y += rabbit_speed_y
 
-    if rabiit.x < 0:
-        rabiit.x = 0
-    if rabiit.x > WIDTH - rabiit.width:
-        rabiit.x = WIDTH - rabiit.width
+    if rabbit.x < 0:
+        rabbit.x = 0
+    if rabbit.x > screen.get_width() - rabbit.width:
+        rabbit.x = screen.get_width()- rabbit.width
 
     food.y += food_speed
 
-    if food.y > screen.get_height() or rabiit.colliderect(food):
-        food.x = random.randint(0, WIDTH - food.width)
+    if food.y > screen.get_height() or rabbit.colliderect(food):
+        food.x = random.randint(0, screen.get_width() - food.width)
         food.y = -3
 
 
-    screen.fill((255, 255, 255)) 
+    screen.fill((0,0 ,0 )) 
 
         
-    screen.blit(rabit_img, rabiit)  # Draw the rabbit image
+    screen.blit(rabit_img, rabbit)  # Draw the rabbit image
     pygame.draw.rect(screen, red, food)
 
     pygame.display.flip()
